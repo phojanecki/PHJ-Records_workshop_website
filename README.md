f07b7cbd4a1b3f4d5feda98f0ee06e6775aae5ba
phojanecki <pawel.ho.janecki@gmail.com>
2016-04-22 19:11:40 +0200

    Adding pages

---
+++ README.md
@@ -0,0 +1,30 @@
+#PHJ Records - workshop website
+
+This is a website of the fake record label PHJ Records.
+It was created for non commercial purposes in order to practice HTML, CSS, Sass, JavaScript and jQuery skills.
+All images included in this project were modified in Photoshop CC 2015
+Your feedback (both positive and negative) will be very welcome :)
+
+Images used within that project were downloaded from:
+* http://agenciacn.com/variedades/coldplay-es-la-banda-mas-galardonada-del-reino-unido/
+* http://the1975.com/love
+* https://www.youtube.com/watch?v=CiP3jC9CQUk
+* http://wallpapers-best.com/1586-die-antwoord.html
+* http://www.allwallpaper.in/de/music-concert-wallpaper-8754.html
+* http://hersplendidplumage.com/
+
+Artists images:
+* http://mad-music.pl/wp-content/uploads/2015/02/G-Wiz.jpg
+* http://uwmpost.com/fringe/concert-preview-the-1975-at-the-rave
+* http://www.koncertomania.pl/newsy/87433-Years-Years-blyszczy.html
+* http://runthetrap.com/2015/05/17/relive-jack-us-set-from-hangout-fest/
+* http://www.theguardian.com/music/2015/dec/04/coldplay-a-head-full-of-dreams-review-a-failure-to-commit-to-pop
+* http://popheart.pl/wp-content/uploads/2015/12/The-Weeknd-at-the-Juno-Aw-009.jpg
+* http://www.ew.com/topic/tyga
+* http://www.hitsync.net/tag/kid-ink/
+* http://runthetrap.com/2016/03/11/with-you-ghost-feat-vince-staples-major-lazer-remix-on-beats-1-lazer-sound/
+
+Album covers:
+* http://www.albumartexchange.com/
+
+All rights to the music and videos used within the project are reserved to the respective owners.
---
+++ js/app.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+
+// Hamburger push left
+  $('#hamburger').on('click', function (){
+    console.log('dziala');
+    $('.wrapper').toggleClass('open');
+  });
+
+// Sticky menu + smooth scrooling to anchor
+$('#menu .inner-link').on('click', function(event){
+  event.preventDefault();
+  $('body').animate({
+    scrollTop: $($(this).attr("href")).offset().top + "px",
+  }, 1500);
+});
+
+// Anchor to specified offset on different subpage
+if (window.location.hash != null && window.location.hash != '') {
+        $('body').animate({
+            scrollTop: $(window.location.hash).offset().top
+        }, 1000);
+}
+
+// News slider
+$("#slideshow > div:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow > div:first')
+  .fadeOut(2000)
+  .next()
+  .fadeIn(2000)
+  .end()
+  .appendTo('#slideshow');
+},  4000);
+
+// Smooth scrolling for 'Music' link
+$('.music').on('click', function(event){
+  event.preventDefault();
+  $('body').animate({
+    scrollTop: $($(this).attr("href")).offset().top + "px"
+  }, 1000);
+});
+
+// Submit form (does not work yet)
+function validateForm() {
+  var $form = $('#delivery-form');
+  var $name = $('#name');
+  var $street = $('#street');
+  var $city = $('#city');
+  var $post = $('#post');
+  var $email = $('#email');
+  var $tel = $('#tel');
+  var $submit = $('#submit');
+
+  $('.simpleCart_checkout').on('click', function(e){
+    // e.preventDefault();
+    var inputName = $(name).val();
+    var inputStreet = $(street).val();
+    var inputCity = $(city).val();
+    var inputPost = $(post).val();
+    var inputEmail = $(email).val();
+    var inputTel = $(tel).val();
+
+    // ((inputName == null || inputName == "")
+    //   && (inputStreet == null || inputStreet == "")
+    //   && (inputCity == null || inputCity == "")
+    //   && (inputPost == null || inputPost == "")
+    //   && (inputEmail == null || inputEmail == "")
+    //   && (inputTel == null || inputTel == ""))
+
+
+    if ((inputName == "")
+      || (inputStreet == "")
+      || (inputCity == "")
+      || (inputPost == "")
+      || (inputEmail == "")
+      || (inputTel = "")) {
+      alert("Please fill in obligatory fields");
+      e.preventDefault();
+    } else {
+      $submit.addClass('simpleCart_checkout');
+      // e.preventDefault();
+      simpleCart({
+        checkout: {
+          type: "PayPal",
+          email: "you@yours.com"
+        }
+      });
+    }
+  });
+}
+// validateForm();
+
+// Show shipping address
+var $showShippingAddress = $('.show-shipping-address');
+var $shippingAddressBox = $('.shipping-address');
+
+$showShippingAddress.on('click', function(){
+  $shippingAddressBox.slideToggle(1000);
+});
+
+
+// SIMPLE CART
+  simpleCart({
+    checkout: {
+      type: "PayPal",
+      email: "you@yours.com"
+    }
+  });
+
+
+//Shipping costs
+  simpleCart.shipping(function(){
+    return 7.99;
+  });
+  simpleCart.shipping();
+
+//Tax rate
+  simpleCart({
+  taxRate: 0.23
+  });
+
+  simpleCart.tax();
+});
